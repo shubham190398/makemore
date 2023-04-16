574a6ba9046b69d326869f14da1c01bdec36f945
Shubham Acharya <shubham190398@gmail.com>
2023-04-16 13:21:30 +0530

    Fixing initial loss blowup

--- makemore_rnn.py
+++ makemore_rnn.py
@@ -73,13 +73,15 @@ def rnn():
     C = Embeddings
     W1, B1 = The Hidden Layer
     W2, B2 = The Output Layer
+    W2 is multiplied by 0.01 to ensure its a small number while B2 is initialized as 0 so that the initial loss
+    is not very big
     """
     g = torch.Generator().manual_seed(42)
     C = torch.randn((vocab_size, n_embd), generator=g)
     W1 = torch.randn((n_embd * block_size, n_hidden), generator=g)
     B1 = torch.randn(n_hidden, generator=g)
-    W2 = torch.randn((n_hidden, vocab_size), generator=g)
-    B2 = torch.randn(vocab_size, generator=g)
+    W2 = torch.randn((n_hidden, vocab_size), generator=g) * 0.01
+    B2 = torch.randn(vocab_size, generator=g) * 0
 
     parameters = [C, W1, B1, W2, B2]
     print("Total number of parameters in this neural network is: ",

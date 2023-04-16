c35ec652048b7ff4d7e2f08320a91917fce635ad
Shubham Acharya <shubham190398@gmail.com>
2023-04-16 20:37:51 +0530

    Added layers

--- makemore_rnn_pytorch.py
+++ makemore_rnn_pytorch.py
@@ -4,6 +4,8 @@ A much more indepth neural network implemented with PyTorch
 
 # Importing dependencies
 import torch
+import random
+import torch.nn.functional as F
 
 
 # Global variable
@@ -70,3 +72,75 @@ class Tanh:
     @staticmethod
     def parameters():
         return []
+
+
+# Helper function to create data
+def create_data(words, s_to_i, block_size):
+    X, Y = [], []
+
+    for w in words:
+        context = [0] * block_size
+
+        for ch in w + '.':
+            idx = s_to_i[ch]
+            X.append(context)
+            Y.append(idx)
+            context = context[1:] + [idx]
+
+    return torch.tensor(X), torch.tensor(Y)
+
+
+# RNN Body
+def rnn():
+    # Load the words and create a torch tensor
+    words = open("names.txt", "r").read().splitlines()
+
+    # Create string to integer and integer to string dictionaries, indexing the . element at position 0
+    chars = sorted(list(set(''.join(words))))
+    s_to_i = {s: i + 1 for i, s in enumerate(chars)}
+    s_to_i['.'] = 0
+    i_to_s = {i: s for s, i in s_to_i.items()}
+    vocab_size = len(i_to_s)
+
+    """
+        Creating the training dataset from the list of words. 85% of the data will be used for training, while 10%
+        will be used for validation and 5% will be used for testing
+    """
+    random.seed(60)
+    random.shuffle(words)
+    n1 = int(0.85 * len(words))
+    n2 = int(0.95 * len(words))
+
+    block_size = 3
+    Xtrain, Ytrain = create_data(words[:n1], s_to_i, block_size)
+    Xval, Yval = create_data(words[n1:n2], s_to_i, block_size)
+    Xtest, Ytest = create_data(words[n2:], s_to_i, block_size)
+
+    # Defining the layers
+    n_emb = 10
+    n_hidden = 100
+
+    C = torch.randn((vocab_size, n_emb), generator=g)
+    layers = [
+        Linear(n_emb * block_size, n_hidden), Tanh(),
+        Linear(n_hidden, n_hidden), Tanh(),
+        Linear(n_hidden, n_hidden), Tanh(),
+        Linear(n_hidden, n_hidden), Tanh(),
+        Linear(n_hidden, n_hidden), Tanh(),
+        Linear(n_hidden, vocab_size),
+    ]
+
+    # Initializing the layers
+    with torch.no_grad():
+        layers[-1].weight *= 0.1
+
+        for layer in layers[:-1]:
+            if isinstance(layer, Linear):
+                layer.weight *= 5/3
+
+    parameters = [C] + [p for layer in layers for p in layer.parameters()]
+    print("Total parameters: ", sum(p.nelement() for p in parameters))
+
+    # Adding gradient computation
+    for p in parameters:
+        p.requires_grad = True

ae768777305b64cb5b22944ac639e439f83d03d7
Shubham Acharya <shubham190398@gmail.com>
2023-04-16 20:19:17 +0530

    Adding class definition for linear layer

---
+++ makemore_rnn_pytorch.py
@@ -0,0 +1,26 @@
+"""
+A much more indepth neural network implemented with PyTorch
+"""
+
+# Importing dependencies
+import torch
+
+
+# Global variable
+g = torch.Generator().manual_seed(1234)
+
+
+# Defining the linear layer
+class Linear:
+    def __init__(self, fan_in, fan_out, bias=True):
+        self.weight = torch.randn((fan_in, fan_out), generator=g) / fan_in**0.5
+        self.bias = torch.zeros(fan_out) if bias else None
+
+    def __call__(self, x):
+        self.out = x @ self.weight
+        if self.bias is not None:
+            self.out += self.bias
+        return self.out
+
+    def parameters(self):
+        return [self.weight] + ([] if self.bias is None else [self.bias])

833b1173363a5d4577d57e806686f73091dfcf91
Shubham Acharya <shubham190398@gmail.com>
2023-04-20 12:31:29 +0530

    Initial commit

---
+++ makemore_wavenet.py
@@ -0,0 +1,56 @@
+"""
+Wavenet framework for generating more text out of a file
+"""
+
+# Importing Dependencies
+import torch
+import torch.nn.functional as F
+import random
+import matplotlib.pyplot as plt
+
+
+# Helper function to create data
+def create_data(words, s_to_i, block_size):
+    X, Y = [], []
+
+    for w in words:
+        context = [0] * block_size
+
+        for ch in w + '.':
+            idx = s_to_i[ch]
+            X.append(context)
+            Y.append(idx)
+            context = context[1:] + [idx]
+
+    return torch.tensor(X), torch.tensor(Y)
+
+
+# Body of Wavenet
+def wavenet():
+
+    # Load the words and create a torch tensor
+    words = open("names.txt", "r").read().splitlines()
+
+    # Create string to integer and integer to string dictionaries, indexing the . element at position 0
+    chars = sorted(list(set(''.join(words))))
+    s_to_i = {s: i + 1 for i, s in enumerate(chars)}
+    s_to_i['.'] = 0
+    i_to_s = {i: s for s, i in s_to_i.items()}
+    vocab_size = len(i_to_s)
+
+    """
+        Creating the training dataset from the list of words. 80% of the data will be used for training, while 10%
+        will be used for validation and 10% will be used for testing
+    """
+    random.seed(42)
+    random.shuffle(words)
+    n1 = int(0.8 * len(words))
+    n2 = int(0.9 * len(words))
+
+    block_size = 3
+    Xtrain, Ytrain = create_data(words[:n1], s_to_i, block_size)
+    Xval, Yval = create_data(words[n1:n2], s_to_i, block_size)
+    Xtest, Ytest = create_data(words[n2:], s_to_i, block_size)
+
+
+wavenet()

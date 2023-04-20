f71a36a2045581f31f333ab2208abd58fd2f511d
Shubham Acharya <shubham190398@gmail.com>
2023-04-20 13:38:26 +0530

    Adding Sequential definition

--- makemore_wavenet.py
+++ makemore_wavenet.py
@@ -76,7 +76,6 @@ class Tanh:
 
 # Definition for embedding
 class Embedding:
-
     def __init__(self, num_embeddings, embedding_dim):
         self.weight = torch.randn((num_embeddings, embedding_dim))
 
@@ -96,6 +95,21 @@ class Flatten:
 
     def parameters(self):
         return []
+
+
+# Definition for Sequential Layer:
+class Sequential:
+    def __init__(self, layers):
+        self.layers = layers
+
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        self.out = x
+        return self.out
+
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
 
 
 # Helper function to create data
